e68bd70fe55b52162344f4dde8891712f732aa0d
heehminh <mink141416@gmail.com>
2023-02-19 21:30:13 +0900

    [FEAT] 할 일 목록을 수정하는 버튼 추가

--- Todo/src/components/List.js
+++ Todo/src/components/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const List = React.memo(
   ({
@@ -11,6 +12,9 @@ const List = React.memo(
     snapshot,
     handleClick,
   }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const handleCompleteChange = (id) => {
       let newToDoData = todoData.map((data) => {
         if (data.id === id) {
@@ -21,32 +25,95 @@ const List = React.memo(
       setTodoData(newToDoData);
     };
 
-    return (
-      <div>
+    const handleEditChange = (event) => {
+      setEditedTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+
+      setTodoData(newTodoData);
+      setIsEditing(false);
+    };
+
+    if (isEditing) {
+      return (
         <div
-          key={id}
-          {...provided.draggableProps}
-          ref={provided.innerRef}
-          {...provided.dragHandleProps}
-          className={`${
-            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-          } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+          className={`flex items-center justify-between w-full px-4 py-1 bg-gray-100 text-gray-600 border rounded`}
         >
-          <input
-            type="checkbox"
-            onChange={() => handleCompleteChange(id)}
-            defaultChecked={false}
-          />
-          <span className={completed && "line-through"}> {title}</span>
-          <button
-            className="px-4 py-2 float-right"
-            onClick={() => handleClick(id)}
-          >
-            x
-          </button>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="w-full px-3 py-2 mr-4 ml-4 text-gray-500 rounded"
+              value={editedTitle}
+              onChange={handleEditChange}
+            />
+          </form>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(false)}
+              type="button"
+            >
+              x
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="px-4 py-2 float-right"
+              type="submit"
+            >
+              save
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <div
+            key={id}
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            className={`${
+              snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+            } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+          >
+            <div>
+              <input
+                className="mr-2"
+                type="checkbox"
+                onChange={() => handleCompleteChange(id)}
+                defaultChecked={completed}
+              />{" "}
+              <span className={completed ? "line-through" : undefined}>
+                {title}
+              </span>
+            </div>
+            <div className="items-center">
+              <button
+                className="float-right px-4 py-2"
+                onClick={() => handleClick(id)}
+              >
+                x
+              </button>
+              <button
+                className="float-right px-4 py-2"
+                onClick={() => setIsEditing(true)}
+              >
+                edit
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 );
 
