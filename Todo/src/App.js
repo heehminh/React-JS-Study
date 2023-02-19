599394279ecbc8c31a38aae9f172177806c99015
heehminh <mink141416@gmail.com>
2023-02-19 20:53:09 +0900

    [FEAT] 할 일 리스트 모두 지우기 버튼 생성

--- Todo/src/App.js
+++ Todo/src/App.js
@@ -32,11 +32,16 @@ export default function App() {
     [todoData]
   ); // 콜백함수, 의존성 배열 (todoData가 변할때 함수를 다시 생성)
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
 
         {/* props로 내려주기 */}
