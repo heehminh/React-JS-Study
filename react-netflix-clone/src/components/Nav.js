196b9a4dbd12567b1bf64f3b3ac003ac2a03692c
heehminh <mink141416@gmail.com>
2023-02-22 11:49:46 +0900

    [FEAT] 네비게이션 바

---
+++ react-netflix-clone/src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  // 스크롤을 50이상 내렸을 때, Nav background: #111
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  });
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
