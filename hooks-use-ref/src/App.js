023e2776b59465cd9a96640faf176ea0b56a18f6
heehminh <mink141416@gmail.com>
2023-02-26 19:14:18 +0900

    Study: useRef로 렌더링 횟수 저장

--- hooks-use-ref/src/App.js
+++ hooks-use-ref/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const App = () => {
   // 예제 1: useState와 useRef 비교
   const [count, setCount] = useState(0);
   const countRef = useRef(0); // countRef= current: 0;
 
-  console.log("렌더링...");
+  // console.log("렌더링...");
   // 버튼을 누를때마다 App 리렌더링
   const increaseCountState = () => {
     setCount(count + 1);
@@ -42,6 +42,17 @@ const App = () => {
 
   // 렌더링이 되면(state를 업데이트) Ref에 저장된 값은 유지되나, let에 저장된 값은 초기화됨
 
+  // 예제 3: useEffect로 render되었을때 카운트 저장
+  // 카운트 저장공간: renderCount(Ref)
+  // 카운트를 State에 저장하면 무한반복으로 오류가 남 (state 업데이트 -> 렌더링 -> state 업데이트 -> 렌더링 -> ...)
+  const [count3, setCount3] = useState(1);
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+    console.log("렌더링 수: ", renderCount.current);
+  });
+
   return (
     <div>
       <div>
@@ -61,6 +72,12 @@ const App = () => {
         <button onClick={increaseRef}>Ref 올려</button>
         <button onClick={increaseVar}>Var 올려</button>
         <button onClick={printResults}>Ref Var 값 출력</button>
+      </div>
+      <div>
+        <br />
+        예제 3:
+        <p>Count: {count3}</p>
+        <button onClick={() => setCount3(count3 + 1)}>올려</button>
       </div>
     </div>
   );
