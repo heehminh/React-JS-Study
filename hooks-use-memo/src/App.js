fcdcccbfa6a58273f81b20fa77ea907cc7f20e5b
heehminh <mink141416@gmail.com>
2023-03-04 12:37:05 +0900

    Study: useMemo를 이용하여 컴포넌트 최적화

---
+++ hooks-use-memo/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useMemo } from "react";
+import "./App.css";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
+
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+
+  useEffect(() => {
+    console.log("useEffect 호출 ");
+  }, [location]);
+
+  return (
+    <div className="App">
+      <h2>하루에 몇끼 먹어요?</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+
+      <hr />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라: {location.country}</p>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+    </div>
+  );
+}
+
+export default App;
