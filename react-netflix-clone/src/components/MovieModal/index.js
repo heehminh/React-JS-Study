d7d5134fc6c1e536356d99060493eb4db0124058
heehminh <mink141416@gmail.com>
2023-03-08 01:17:07 +0900

    [FEAT] 모달 창 외부 클릭시 모달 close

--- react-netflix-clone/src/components/MovieModal/index.js
+++ react-netflix-clone/src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useOnClickOutside } from "../../hooks/useOnClickOutside";
 import "./MovieModal.css";
 
 const MovieModal = ({
@@ -11,10 +12,15 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span className="modal-close" onClick={() => setModalOpen(false)}>
             X
           </span>
