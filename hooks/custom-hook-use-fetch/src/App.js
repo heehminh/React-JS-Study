8b93c3164757b86bbbd96e12e2db96cfa3a34003
heehminh <mink141416@gmail.com>
2023-03-07 20:47:05 +0900

    Study: custom hook(useFetch) - users, posts, todos 첫번째 데이터

--- hooks/custom-hook-use-fetch/src/App.js
+++ hooks/custom-hook-use-fetch/src/App.js
@@ -3,15 +3,16 @@ import { useFetch } from "./useFetch";
 const baseUrl = "https://jsonplaceholder.typicode.com";
 
 function App() {
-  const { data, fetchUrl } = useFetch(baseUrl, "users");
+  const { data: userData } = useFetch(baseUrl, "users");
+  const { data: postData } = useFetch(baseUrl, "posts");
+  const { data: todoData } = useFetch(baseUrl, "todos");
 
   return (
     <div>
       <h1>useFetch</h1>
-      <button onClick={() => fetchUrl("users")}>Users</button>
-      <button onClick={() => fetchUrl("posts")}>Posts</button>
-      <button onClick={() => fetchUrl("todos")}>Todos</button>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {userData && <pre>{JSON.stringify(userData[0], null, 2)}</pre>}
+      {postData && <pre>{JSON.stringify(postData[0], null, 2)}</pre>}
+      {todoData && <pre>{JSON.stringify(userData[0], null, 2)}</pre>}
     </div>
   );
 }
