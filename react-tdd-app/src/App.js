3ab76b4696c0226ff25c940136dace1bc42e53dc
heehminh <mink141416@gmail.com>
2023-03-11 18:37:19 +0900

    [TEST] 플러스, 마이너스 작동시 카운터 변화 테스트

--- react-tdd-app/src/App.js
+++ react-tdd-app/src/App.js
@@ -8,8 +8,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <button data-testid="minus-button">-</button>
-        <button data-testid="plus-button">+</button>
+        <button
+          data-testid="minus-button"
+          onClick={() => setCount((prev) => prev - 1)}
+        >
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          +
+        </button>
       </header>
     </div>
   );
--- react-tdd-app/src/App.test.js
+++ react-tdd-app/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counter starts at 0", () => {
@@ -17,4 +17,12 @@ test("plus button has correct test", () => {
   render(<App />);
   const buttonElement = screen.getByTestId("plus-button");
   expect(buttonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
 });
