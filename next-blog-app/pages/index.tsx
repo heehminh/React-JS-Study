a8e000f40f8083dff9cc455889179f1ec6be5e05
heehminh <mink141416@gmail.com>
2023-03-21 01:54:32 +0900

    [FEAT] 포스트 리스트 나열

--- next-blog-app/pages/index.tsx
+++ next-blog-app/pages/index.tsx
@@ -1,8 +1,17 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import homeStyles from "@/styles/Home.module.css";
+import { getSortedPostsData } from "@/lib/posts";
 
-const Home: NextPage = () => {
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
   return (
     <div className={homeStyles.container}>
       <Head>
@@ -16,10 +25,27 @@ const Home: NextPage = () => {
 
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}>{/* 블로그 글을 넣어준다 */}</ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
