1a928545128d50a930975444c926065a264e5edb
heehminh <mink141416@gmail.com>
2023-03-28 17:50:40 +0900

    API- useReducer 요청상태관리

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,45 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  /** error, loading, user 초기화 + get */
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      dispatch({ type: "SUCCESS", data: response.data });
+      console.log("다시불러오기!!");
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
 
   useEffect(() => {
-    /** error, loading, user 초기화 + get */
-    const fetchUsers = async () => {
-      try {
-        // 요청이 시작할 때 error, users 초기화
-        setError(null);
-        setUsers(null);
-        // loading 상태를 true 로
-        setLoading(true);
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
-
     fetchUsers();
   }, []);
 
+  const { loading, data: users, error } = state;
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러발생</div>;
   if (!users) return null;
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시불러오기</button>
+    </div>
   );
 }
 
