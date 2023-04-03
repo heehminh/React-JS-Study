b3da05d356e3fe832c15ef87aa1f4317c50d6fc0
heehminh <mink141416@gmail.com>
2023-04-04 01:30:24 +0900

    toolkit: 비동기 상태관리

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import store from "./store";
-import { up } from "./counterSlice";
+import { asyncUpFetch, up } from "./counterSlice";
 
 function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state) => {
     return state.counter.value;
   });
+  const status = useSelector((state) => {
+    return state.counter.status;
+  });
   return (
     <div>
       <button
         onClick={() => {
           // dispatch({ type: "counterSlice/up", step: 2 });
-          dispatch(up(2)); // payload: 2
+          dispatch(up(1)); // payload: 1
         }}
       >
         +
       </button>
-      {count}
+      <button
+        onClick={() => {
+          dispatch(asyncUpFetch());
+        }}
+      >
+        + async fetch
+      </button>
+      <br />
+      {count} | {status}
     </div>
   );
 }
--- react-redux-app/src/counterSlice.js
+++ react-redux-app/src/counterSlice.js
@@ -1,14 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+const asyncUpFetch = createAsyncThunk(
+  "counterSlice/asyncUpFetch", // action type
+  async () => {
+    const resp = await fetch(
+      "https://api.countapi.xyz/hit/opesaljkdfslkjfsadf.com/visits"
+    );
+    const data = await resp.json();
+    return data.value;
+  }
+);
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { value: 0 },
+  initialState: { value: 0, status: "Welcome" },
   reducers: {
     up: (state, action) => {
       state.value = state.value + action.payload;
     },
   },
+  extraReducers: (builder) => {
+    // 비동기 작업은 extraReducers 사용
+    builder.addCase(asyncUpFetch.pending, (state, action) => {
+      state.status = "Loading";
+    });
+    builder.addCase(asyncUpFetch.fulfilled, (state, action) => {
+      state.value = action.payload;
+      state.status = "complete";
+    });
+    builder.addCase(asyncUpFetch.rejected, (state, action) => {
+      state.status = "fail";
+    });
+  },
 });
 
 export default counterSlice;
 export const { up } = counterSlice.actions;
+export { asyncUpFetch };
