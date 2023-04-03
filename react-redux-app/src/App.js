ef535c69b9ac3d279ac9fae0bbad7b87e83bb4f7
heehminh <mink141416@gmail.com>
2023-04-04 00:39:43 +0900

    toolkit: slice, store 파일 분리

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,30 +1,7 @@
 import React from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { value: 0 },
-  reducers: {
-    up: (state, action) => {
-      state.value = state.value + action.payload;
-    },
-  },
-});
-
-// function reducer(state, action) {
-//   if (action.type === "up") {
-//     return { ...state, value: state.value + action.step };
-//   }
-//   return state;
-// }
-
-// const initialState = { value: 0 };
-const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-  },
-});
+import store from "./store";
+import { up } from "./counterSlice";
 
 function Counter() {
   const dispatch = useDispatch();
@@ -36,7 +13,7 @@ function Counter() {
       <button
         onClick={() => {
           // dispatch({ type: "counterSlice/up", step: 2 });
-          dispatch(counterSlice.actions.up(2)); // payload: 2
+          dispatch(up(2)); // payload: 2
         }}
       >
         +
