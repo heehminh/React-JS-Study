c66306c9ffcf0940fc10d81714398c5b3be9023a
heehminh <mink141416@gmail.com>
2023-04-05 22:27:04 +0900

    MUI: Sign In UI

---
+++ mui-login-app/src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export default function App() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <TextField
+          margin="normal"
+          label="Email Address"
+          required
+          fullWidth
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          label="Password"
+          type="password"
+          required
+          fullWidth
+          name="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign in
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid item>
+            <Link>Don't have an account? Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
